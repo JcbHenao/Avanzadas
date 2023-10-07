270587a1edb4615e1dc6b134486117446abf4146
JcbHenao <146904680+JcbHenao@users.noreply.github.com>
2023-10-07 13:43:42 -0500

    Update Servicios.java

--- src/main/java/Servicios/Servicios.java
+++ src/main/java/Servicios/Servicios.java
@@ -1,15 +1,29 @@
 package Servicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servicios {
 
     int codServicio;
     String nombreServicio;
     int precioServicio;
+    private static int numservicios = 0;
+    private static List<Integer> codigosServiciosExistentes = new ArrayList<>();
 
     public Servicios(int codServicio, String nombreServicio, int precioServicio) {
+        verificarCodigoServicio(codServicio);
         this.codServicio = codServicio;
         this.nombreServicio = nombreServicio;
         this.precioServicio = precioServicio;
+        codigosServiciosExistentes.add(codServicio);
+        numservicios++;
+    }
+
+    private void verificarCodigoServicio(int codServicio) {
+        if (codigosServiciosExistentes.contains(codServicio)) {
+            throw new IllegalArgumentException("El código de servicio ya está asignado a otro servicio.");
+        }
     }
 
     public int getCodServicio() {
@@ -36,4 +50,21 @@ public class Servicios {
         this.precioServicio = precioServicio;
     }
 
+    public static int getNumservicios() {
+        return numservicios;
+    }
+
+    public void actualizarServicio(int nuevoCodServicio, String nuevoNombreServicio, int nuevoPrecioServicio) {
+        verificarCodigoServicio(nuevoCodServicio);
+        setCodServicio(nuevoCodServicio);
+        setNombreServicio(nuevoNombreServicio);
+        setPrecioServicio(nuevoPrecioServicio);
+    }
+
+    @Override
+    public String toString() {
+        return "Servicios{" + "codServicio=" + codServicio + ", nombreServicio=" + nombreServicio + ", precioServicio=" + precioServicio + '}';
+    }
+
 }
+
